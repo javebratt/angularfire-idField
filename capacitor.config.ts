987418b9e097371b14dc4a249ec61cf3ee4fe034
Jorge Vergara <jorge@jorgevergara.co>
2022-07-31 11:13:58 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'angularfire-id',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
