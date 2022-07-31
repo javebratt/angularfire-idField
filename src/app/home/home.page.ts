6a24281018f29ca93c298bbbf310a52063a771da
Jorge Vergara <jorge@jorgevergara.co>
2022-07-31 15:16:06 -0400

    feat: Added the 2 methods for the IDs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {
+  collection,
+  collectionChanges,
+  Firestore,
+  collectionData,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +14,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  readonly items$: Observable<FoodItem[]> = collectionData(
+    collection(this.firestore, 'foodList'),
+    { idField: 'id' }
+  ) as Observable<FoodItem[]>;
 
-  constructor() {}
+  readonly items2$: Observable<FoodItem[]> = collectionChanges(
+    collection(this.firestore, 'foodList')
+  ).pipe(
+    map((items) => items.map( (item) => {
+      const data = item.doc.data() as FoodItem;
+      const id = `prefix-${item.doc.id}`;
 
+      return { id, ...data }
+    }))
+  );
+
+  constructor(private readonly firestore: Firestore) {}
+}
+
+interface FoodItem {
+  id: string;
+  name: string;
 }
